4421d7a0d2a986b024d7d7cf799183afec9dd18b
Anand Bajpai <anand.bajpai@1mg.com>
2023-01-19 17:53:14 +0530

    AnalyticsData initializer

--- RudderEvents/RudderEvents/Listing/AnalyticsDataModel.swift
+++ RudderEvents/RudderEvents/Listing/AnalyticsDataModel.swift
@@ -11,6 +11,25 @@ public class AnalyticsData: NSObject {
   
   public override init() {}
   
+  @objc
+  public init(category: String?,
+              action: String?,
+              label: String?,
+              customDimention: [String: String]?) {
+    if let category = category {
+      self.category = category
+    }
+    if let action = action {
+      self.action = action
+    }
+    if let label = label {
+      self.label = label
+    }
+    if let customDimention = customDimention {
+      self.customData = customDimention
+    }
+  }
+  
   @objc public var category = "To test"
   @objc public var action = ""
   @objc public var label = ""
